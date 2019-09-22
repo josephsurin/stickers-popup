6e465090cb1e5d113d145c0570f1dd71df6f239c
Joseph Surin <joseph.surin@gmail.com>
2019-09-22 19:01:37 +1000

    notebook shift h/l to switch page

--- src/stickers_notebook.py
+++ src/stickers_notebook.py
@@ -1,10 +1,23 @@
-from gi.repository import Gtk
+from gi.repository import Gtk, GdkPixbuf, Gdk
 
 from stickers_grid import stickers_grid
+from util import PREVIEW_WIDTH, KEYCODES
 
 def stickers_notebook(stickers_data):
     notebook = Gtk.Notebook()
     for sd in stickers_data:
-        title_img = Gtk.Image.new_from_file(sd['stickers_data'][0]['preview_file'])
+        title_img_pb = GdkPixbuf.Pixbuf.new_from_file_at_scale(sd['stickers_data'][0]['preview_file'], PREVIEW_WIDTH//2, -1, True)
+        title_img = Gtk.Image.new_from_pixbuf(title_img_pb)
         notebook.append_page(stickers_grid(sd['stickers_data']), title_img)
+
+    notebook.connect('key_press_event', handle_keypress)
+
     return notebook
+
+def handle_keypress(notebook, e):
+    if e.state == Gdk.ModifierType.SHIFT_MASK:
+        keycode = e.hardware_keycode
+        if keycode == KEYCODES['h']:
+            notebook.prev_page()
+        elif keycode == KEYCODES['l']:
+            notebook.next_page()
